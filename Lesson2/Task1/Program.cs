a134719b2d85dfb31749e343ad6bd0563435d209
Ivga <borozdinaes89@gmail.com>
2022-12-11 17:01:39 +0500

    Lesson 2 task 1 is over

--- Lesson2/Task1/Program.cs
+++ Lesson2/Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. Не использовать строки
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine($"Enter a three-digit number: ");  //число один раз требуется, поэтому не стала использовать функцию/метод
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 100 && n < 1000) {
+    int div = n / 10;
+    int rem = div % 10;
+    System.Console.WriteLine($"The second digit of number {n} is {rem}.");
+}
+else {
+    System.Console.WriteLine("You entered not a three-digit number.");
+}
