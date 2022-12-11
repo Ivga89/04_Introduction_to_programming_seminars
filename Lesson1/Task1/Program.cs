7383ac49a8006772e5874bbda09525d409fd42a7
Ivga <borozdinaes89@gmail.com>
2022-12-11 16:17:32 +0500

    Lesson 1 is over

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int Prompt(string message)
+{
+    System.Console.WriteLine($"{message} >");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int n1 = Prompt("Enter the first number: ");
+int n2 = Prompt("Enter the second number: ");
+
+if (n1 > n2)
+{
+    System.Console.WriteLine($"The first number ({n1}) is bigger than the second ({n2})");
+}
+
+else
+{
+    System.Console.WriteLine($"The second number ({n2}) is bigger than the first ({n1})");
+}
---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int Prompt(string message) {
+    System.Console.WriteLine($"{message} > ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int n1 = Prompt("Enter the first number: ");
+int n2 = Prompt("Enter the second number: ");
+int n3 = Prompt("Enter the third number: ");
+int max = n1;
+
+if (max < n2) max = n2;
+if (max < n3) max = n3;
+
+System.Console.WriteLine($"The biggest number is {max}.");
