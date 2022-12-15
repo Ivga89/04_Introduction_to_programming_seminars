6b2ee1c3e35135724d3aeafab352d2149c91812c
Ivga <borozdinaes89@gmail.com>
2022-12-15 14:28:58 +0500

    Lesson 3 task 1 ready

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    System.Console.WriteLine($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Paladron(int n)
+{
+    if ((n / 10000) == (n % 10) & ((n / 1000) % 10) == ((n % 100) / 10))
+    {
+        System.Console.WriteLine("The number is a paladron");
+    }
+    else
+    {
+        System.Console.WriteLine("The number is not a paladron");
+    }
+}
+
+int n = Prompt("Enter a five-digit number");
+if (n >= 10000 && n < 99999)
+{
+    Paladron(n);
+}
+else
+{
+    System.Console.WriteLine("You entered not a five-digit number!");
+}
