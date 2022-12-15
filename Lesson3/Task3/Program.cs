35908349d520264f3bec5f22da2c42a9e3b82b48
Ivga <borozdinaes89@gmail.com>
2022-12-15 15:18:22 +0500

    Lesson 3 task 3 ready

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.WriteLine($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = Prompt("Enter a number");
+
+for (int i = 1; i <= n; i++) {
+    System.Console.Write($"{i * i * i} ");
+}
