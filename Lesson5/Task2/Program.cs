38b70e6801849535267c55e6e1133cb5eeff35da
Ivga <borozdinaes89@gmail.com>
2022-12-24 21:33:36 +0500

    Lesson 5 task 2 ready

---
+++ Lesson5/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] RandomArray (int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(0, 100);
+        System.Console.Write($"{array[i]} ");
+    } return array;
+}
+
+int SumOfOddElements(int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++) {
+        if (array[i] % 2 == 1) {
+            sum = sum + array[i];
+        }
+    } return sum;
+}
+
+int randomElemOfArray = new Random().Next(5, 16);
+int[] array = RandomArray(new int[randomElemOfArray]);
+int sumOfOddElements = SumOfOddElements(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"The summ of odd elements of array is: {sumOfOddElements}");
