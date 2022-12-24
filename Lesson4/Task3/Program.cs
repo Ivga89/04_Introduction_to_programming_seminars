68acd40a882028fe075d74884cba0f2da882fe44
Ivga <borozdinaes89@gmail.com>
2022-12-24 19:38:19 +0500

    Lesson 4 task 3 ready

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Напишите программу, которая выводит массив из 8 элементов, заполненный случайными числами. 
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах). Реализовать через функции. 
+// (* Доп сложность, “введите количество элементов массива”, “Введите минимальный порог случайных значений”, 
+// “Введите максимальный порог случайных значений”)
+
+int Prompt(string message)
+{
+    System.Console.WriteLine($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+int countOfArrayElements = Prompt("Enter the count of elements of array");
+int min = Prompt("Enter the min of values");
+int max = Prompt("Enter the max of values");
+int[] array = new int[countOfArrayElements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+        System.Console.Write($"{array[i]} ");
+    }
