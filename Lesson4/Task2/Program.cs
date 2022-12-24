7f8845d8af05a91342c78387ec357c9e3bb0b6ed
Ivga <borozdinaes89@gmail.com>
2022-12-24 18:43:14 +0500

    Lesson 4 task 2 ready

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число
+//  и выдаёт сумму цифр в числе. Реализовать через функции.
+
+int Prompt (string message) {
+    System.Console.WriteLine($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumOfDigits (int a) {
+    int sum = 0;
+    while (a > 0) {
+        sum = sum + (a % 10);
+        a = a / 10;
+    }
+    return sum;
+}
+
+int a = Prompt("Enter number: ");
+int sum = SumOfDigits(a);
+System.Console.WriteLine($"The sum of digits in number {a} is equal to {sum}");
