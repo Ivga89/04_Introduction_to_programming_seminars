2671dbbb154f215937ac7e12233aa9f8183297c4
Ivga <borozdinaes89@gmail.com>
2023-01-01 19:44:18 +0500

    Lesson 6 task 1 ready

--- Lesson5/Task2/Program.cs
+++ Lesson5/Task2/Program.cs
@@ -13,7 +13,7 @@ int[] RandomArray (int[] array) {
 int SumOfOddElements(int[] array) {
     int sum = 0;
     for (int i = 0; i < array.Length; i++) {
-        if (array[i] % 2 == 1) {
+        if (array[i] % 2 == 0) {
             sum = sum + array[i];
         }
     } return sum;
---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    System.Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] ArrayEnrty(int count) {
+    int[] array = new int[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Enter number {i + 1}: ");
+    } return array;
+}
+
+void Print(int[] array)
+{
+    System.Console.Write("Our array: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    } System.Console.WriteLine();
+}
+
+int AbobeZero (int[] array) {
+    int count = 0;
+    foreach (int i in array)
+    {
+        if(i > 0) count++;
+    } return count;
+}
+
+string Plural(int number) {
+    string word = "numbers";
+    if (number == 1) {
+        word = "number";
+    } return word;
+}
+
+int count = Prompt("How many numbers you are going to enter?");
+int[] newArray = ArrayEnrty(count);
+Print(newArray);
+int aboveZero = AbobeZero(newArray);
+string plural = Plural(aboveZero);
+System.Console.WriteLine($"{aboveZero} {plural} In the array Is more then zero.");
