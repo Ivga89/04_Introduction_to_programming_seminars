98d01b1caeb47c01e30a021f8c0d0a4319c4bf0f
Ivga <borozdinaes89@gmail.com>
2023-01-07 21:30:11 +0500

    Lesson 9 task 2 ready

---
+++ Lesson9/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumOfNaturalElements(int m, int n)
+{
+    if (n == m)
+    {
+        return n;
+    }
+    return n + SumOfNaturalElements(m, n - 1);
+}
+
+System.Console.WriteLine(SumOfNaturalElements(1, 15));
+System.Console.WriteLine(SumOfNaturalElements(4, 8));
+System.Console.WriteLine(SumOfNaturalElements(1, 100));
