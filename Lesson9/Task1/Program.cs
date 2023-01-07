d99c97dff99d8f5bebf1ab150540dc04b5f6a4c9
Ivga <borozdinaes89@gmail.com>
2023-01-07 21:19:12 +0500

    Lesson 9 task 1 ready

---
+++ Lesson9/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+void PrintEvenNumbers(int m, int n) {
+    if (n <= m) {
+        return;
+    }
+    if (n % 2 == 1) {
+        n--;
+    }
+    PrintEvenNumbers(m, n - 1);
+    System.Console.Write($"{n} ");
+}
+
+PrintEvenNumbers(1, 5);
+System.Console.WriteLine();
+PrintEvenNumbers(4, 8);
